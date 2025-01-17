c28d84f91d6b6f8a4412d4f065e43103b32f490f
ëstevao97i <pedro.cordova2002@gmail.com>
2025-01-16 23:13:40 -0300

    initial commit with simpleTimer created

---
+++ simple-camel-p/src/main/java/com/estevao/simple_camel/SimpleCamelApplication.java
@@ -0,0 +1,13 @@
+package com.estevao.simple_camel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCamelApplication.class, args);
+	}
+
+}
